3ea29550d318c42ba2f1cd534adfc7c728aafa65
Adam SOudani <asoudani@email.com>
2020-03-23 23:45:45 -0700

    link firebase

---
+++ train.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD9XVmdKAk7JtmD3P3AszdHtk78oe6azu0",
+    authDomain: "train-scheduler-790da.firebaseapp.com",
+    databaseURL: "https://train-scheduler-790da.firebaseio.com",
+    projectId: "train-scheduler-790da",
+    storageBucket: "train-scheduler-790da.appspot.com",
+    messagingSenderId: "429237750413",
+    appId: "1:429237750413:web:39cb88436eec9fb7a930c4",
+    measurementId: "G-1ESE7KZGGE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
